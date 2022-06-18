6ab7e53107e274a337b5160ccf0fdf9d96c3e28b
熊谷柊弥 <ksuimrao@gmail.com>
2022-06-18 09:32:31 +0900

    自社情報編集画面へのルーティング作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OwnCompanyEditController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,5 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/own_company_edit', [OwnCompanyEditController::class, 'index']);
